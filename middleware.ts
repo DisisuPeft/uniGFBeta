b62c36b4c251cb4a3021b70a25395dea102e7177
Daniel <defp240899@gmail.com>
2026-04-23 09:49:24 -0600

    first commit

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  const { pathname } = url;
+
+  if (pathname === "/") {
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+}
+
+export const config = {
+  matcher: ["/"],
+};
