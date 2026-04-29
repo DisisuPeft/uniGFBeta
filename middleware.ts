3d7fb26522ca4a0c67f624837503d8c8f0020629
Daniel <defp240899@gmail.com>
2026-04-29 09:35:18 -0600

    fix middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export default function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone();
-  // const { pathname } = url;
-
-  // if (pathname === "/") {
-  //   url.pathname = "/login";
-  //   return NextResponse.redirect(url);
-  // }
-  return NextResponse.redirect(url);
+  return NextResponse.next();
 }
 
 export const config = {
